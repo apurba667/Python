4385b156f57f883b37ac4873d7e3bc4895626a53
Apurba Roy Anik <apurbaanikroy@gmail.com>
2022-03-11 20:41:47 +0600

    trying

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Hello there! What is your name:")
+name = input()
+print("Hello "+name)
+print("How old are you?")
+age = input()
+birth_year = 2022-int(age)
+st_year = str(birth_year)
+print("You are born in "+st_year)
